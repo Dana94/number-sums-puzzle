cad43d66b91cee454b6988f385ec1947cf43ec4c
Dana <dana.ottaviani@gmail.com>
2021-05-27 14:14:22 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="btns">
+        <button>A</button>
+        <button>B</button>
+        <button>C</button>
+      </div>
+
+      <div className="nums">
+        <div>
+          3
+        </div>
+        <div>
+          4
+        </div>
+      </div>
     </div>
   );
 }
